3d03060a2002452edcafe27dd5f62753f24027ba
_.Yeon._.Gang._ <42836692+nayeonkyoung@users.noreply.github.com>
2020-06-18 15:44:56 +0900

    Add files via upload

--- MemberSort/main.c
+++ MemberSort/main.c
@@ -84,50 +84,61 @@ int main() {
     Member *t;
     t = (Member*)malloc(sizeof(Member) * 10);
     int index = 0;
+    printf("이름\t  사번\t  생년월일\t부서명\t 전화번호\t \n");
     while (1) {
         fscanf_s(fp, "%s %s %s %s %s", t[index].name, MAX_LEN, t[index].companyNm, MAX_LEN, t[index].birth, MAX_LEN, t[index].team, MAX_LEN, t[index].phone, MAX_LEN);
         if (feof(fp))break;
-        printf("%s %s %s %s %s\n", t[index].name, t[index].companyNm, t[index].birth, t[index].team, t[index].phone);
+        printf("%s\t %s\t %s\t %s\t %s\t\n", t[index].name, t[index].companyNm, t[index].birth, t[index].team, t[index].phone);
         index++;
 
     }
-    printf("정렬을 희망하는 키워드를 선택해 주세요!\n");
-    printf("(1. 이름   2. 사번   3. 생년월일   4. 부서명   5. 전화번호)\n");
-    scanf_s("%d", &choice);
+    while (1) {
+        printf("정렬을 희망하는 키워드를 선택해 주세요!\n");
+        printf("(1. 이름   2. 사번   3. 생년월일   4. 부서명   5. 전화번호 0. 종료)\n");
+        scanf_s("%d", &choice);
 
-    switch (choice) {
-    case 1:
-        Nameup(t);
-        for (int i = 0; i < 10; i++) {
-            printf("%s %s %s %s %s\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
+        switch (choice) {
+        case 0 :
+            exit(1);
+            break;
+        case 1:
+            Nameup(t);
+            printf("이름\t  사번\t  생년월일\t부서명\t 전화번호\t \n");
+            for (int i = 0; i < 10; i++) {
+                printf("%s\t %s\t %s\t %s\t %s\t\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
+            }
+            break;
+        case 3:
+            birthup(t);
+            printf("이름\t  사번\t  생년월일\t부서명\t 전화번호\t \n");
+            for (int i = 0; i < 10; i++) {
+                printf("%s\t %s\t %s\t %s\t %s\t\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
+            }
+            break;
+        case 4:
+            teamup(t);
+            printf("이름\t  사번\t  생년월일\t부서명\t 전화번호\t \n");
+            for (int i = 0; i < 10; i++) {
+                printf("%s\t %s\t %s\t %s\t %s\t\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
+            }
+            break;
+        case 5:
+            phoneup(t);
+            printf("이름\t  사번\t  생년월일\t부서명\t 전화번호\t \n");
+            for (int i = 0; i < 10; i++) {
+                printf("%s\t %s\t %s\t %s\t %s\t\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
+            }
+            break;
+        case 2:
+            companyNmup(t);
+            printf("이름\t  사번\t  생년월일\t부서명\t 전화번호\t \n");
+            for (int i = 0; i < 10; i++) {
+                printf("%s\t %s\t %s\t %s\t %s\t\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
+            }
+            break;
         }
-        break;
-    case 3:
-        birthup(t);
-        for (int i = 0; i < 10; i++) {
-            printf("%s %s %s %s %s\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
-        }
-        break;
-    case 4:
-        teamup(t);
-        for (int i = 0; i < 10; i++) {
-            printf("%s %s %s %s %s\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
-        }
-        break;
-    case 5:
-        phoneup(t);
-        for (int i = 0; i < 10; i++) {
-            printf("%s %s %s %s %s\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
-        }
-        break;
-    case 2:
-        companyNmup(t);
-        for (int i = 0; i < 10; i++) {
-            printf("%s %s %s %s %s\n", t[i].name, t[i].companyNm, t[i].birth, t[i].team, t[i].phone);
-        }
-        break;
-
     }
+    
     fclose(fp);
     return 0;
 }
